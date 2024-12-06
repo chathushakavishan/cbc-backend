0216490724d1de96b79dd2761f64c0740f959508
chathushakavishan <chathusha.kavishan@gmail.com>
2024-12-07 01:17:23 +0530

    Create Product Model

--- models/product.js
+++ models/product.js
@@ -1,11 +1,41 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
-  name : String,
-  price : Number,
-  description : String
+const productschema = mongoose.Schema({
+    productId : {
+        type : String,
+        required : true,
+        unique : true,
+    },
+    productName : {
+        type : String,
+        required : true 
+    },
+    altNames : [{
+        type : String
+    }],
+    images : [
+        {
+            type : String
+        }
+    ],
+    price : {
+        type : Number,
+        required : true
+    },
+    lastPrice : {
+        type : Number,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true 
+    },
+    description : {
+        type : String,
+        required : true 
+    }
 })
 
-const Product = mongoose.model("products",productSchema)
+const Product = mongoose.model("products",productSchema);
 
 export default Product;
