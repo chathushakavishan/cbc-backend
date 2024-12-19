c694773eb0b75aea57ccc65b9d651cee7f4b2e68
chathushakavishan <chathusha.kavishan@gmail.com>
2024-12-19 17:15:08 +0530

    create function createProduct

---
+++ controllers/productController.js
@@ -0,0 +1,26 @@
+import Product from "../models/product.js";
+import { isAdmin } from "./userController";
+
+export function createProduct(req,res){
+
+    if(!isAdmin(req)){
+        res.json({
+            message : "Please logis as administrator to add products"
+        })
+        return
+    }
+
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>{
+        res.json({
+            message: "Product created"
+        })
+    }).catch((error)=>{
+        res.json({
+            message: "Product not created"
+        })
+    })
+}
