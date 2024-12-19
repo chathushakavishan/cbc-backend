d5f09fd99a437a1c84be3e232f573e0c461ae4e0
chathushakavishan <chathusha.kavishan@gmail.com>
2024-12-19 17:32:42 +0530

    create product router

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import productRouter from './routes/productRouter.js';
 
 dotenv.config()
 
@@ -46,6 +47,8 @@ app.use(
 )
 
 app.use("/api/users",userRouter)
+app.use("/api/product",productRouter)
+
 app.listen(
   5000,
   ()=>{
